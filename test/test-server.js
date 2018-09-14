ff7f0040766eda59fcc03e2bdd2fe98ec006710a
Timothy <superfluous11@gmail.com>
2018-09-14 09:52:47 -0700

    completed test for recipes GET endpoint

--- test/test-server.js
+++ test/test-server.js
@@ -150,3 +150,32 @@ describe("Shopping List", function() {
     );
   });
 });
+
+
+describe('Recipe List', function(){
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should list items on GET', function(){
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res){
+
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.at.least(1);
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(item) {
+          expect(item).to.be.a('object');
+          expect(item).to.include.keys(expectedKeys);
+        });
+      });
+  });
+});
